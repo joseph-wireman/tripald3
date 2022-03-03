b887420e0715fad270ef19154fc2b54ed8e53521
joe <joewireman7198@gmail.com>
2022-03-03 08:38:21 -0500

    test

--- js/tripalD3.scatterHorizontal.js
+++ js/tripalD3.scatterHorizontal.js
@@ -39,12 +39,7 @@ tripalD3.scatterHorizontal = {
 
         //Generate the threshold lines' attributes
 
-        var lineAttributes = {
-        'x1': function(d) {return d.x1;},
-        'y1': function(d) {return d.y1;},
-        'x2': function(d) {return d.x2;},
-        'y2': function(d) {return d.y2;}
-      };
+
 
 
         //create and append datapoints
@@ -64,6 +59,37 @@ tripalD3.scatterHorizontal = {
         svg.append("g")
                 .attr("class", "y axis")
                 .attr("transform", "translate(0",+options.height+")")
-                .call(yAxis)
-    }
+                .call(yAxis);
+
+        var lineAttributes = {
+                'x1': function(d) {return d.x1;},
+                'y1': function(d) {return d.y1;},
+                'x2': function(d) {return d.x2;},
+                'y2': function(d) {return d.y2;}
+        
+      };
+
+        var drag = d3.behavior.drag()
+          .origin(function(d) {return d;})
+          .on('drag', dragged);
+
+        var line1 = svg.append('line').data(upperThresholdOrigin).attr(lineAttributes).call(drag);
+        function dragged() {
+                var x = d3.event.dx;
+                var y = d3.event.dy;
+                var line = d3.select(this);
+              
+                //Convert svg coordinates to x-axis coordinates
+                var xAxisScale = d3.scale.linear().domain([0, options.width]).range([min, max]);
+                var upperLinePosition = Math.floor(xAxisScale(line1.attr("x2")));
+                var attributes = {
+                        x1: parseInt(line.attr('x1')) + x,
+                        y1: parseInt(line.attr('y1')),
+          
+                        x2: parseInt(line.attr('x2')) + x,
+                        y2: parseInt(line.attr('y2')),
+                    };
+                }
+
+        }
 }
